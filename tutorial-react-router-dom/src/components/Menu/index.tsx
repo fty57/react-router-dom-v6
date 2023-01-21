48dd4ed108bf800c01668e2b3e600eb523ce7e05
Álisson Véras <59287050+fty57@users.noreply.github.com>
2023-01-21 13:36:51 -0300

    92. useParams e useSearchParams

--- tutorial-react-router-dom/src/components/Menu/index.tsx
+++ tutorial-react-router-dom/src/components/Menu/index.tsx
@@ -7,7 +7,15 @@ export const Menu = () => {
       <ul>
         <li>
           <Link to="/">Home</Link>
+        </li>
+        <li>
           <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/posts">Posts</Link>
+        </li>
+        <li>
+          <Link to="/posts/10">Posts 10</Link>
         </li>
       </ul>
     </nav>
---
+++ tutorial-react-router-dom/src/components/Post/index.tsx
@@ -0,0 +1,15 @@
+import { useParams, useSearchParams } from "react-router-dom";
+
+export const Post = () => {
+  const params = useParams();
+  const { id } = params;
+  const [qs, setQs] = useSearchParams();
+
+  return (
+    <div>
+      <h1>
+        Post {`Para: ${id}`} {`QS: ${qs}`}
+      </h1>
+    </div>
+  );
+};
