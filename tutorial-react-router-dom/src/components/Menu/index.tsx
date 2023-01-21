4a9af700caaa53f38fa2014302adb73b6bf9a080
Álisson Véras <59287050+fty57@users.noreply.github.com>
2023-01-21 13:26:24 -0300

    91. Usando Links: Link vs a

---
+++ tutorial-react-router-dom/src/components/Menu/index.tsx
@@ -0,0 +1,15 @@
+import "./style.css";
+import { Link } from "react-router-dom";
+
+export const Menu = () => {
+  return (
+    <nav className="menu">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
