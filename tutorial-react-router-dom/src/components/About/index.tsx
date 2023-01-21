2b32c129a6257ae388a6e73545d1f07a559c7d28
Álisson Véras <59287050+fty57@users.noreply.github.com>
2023-01-21 13:19:57 -0300

    90. Criando as primeiras rotas

--- tutorial-react-router-dom/src/components/About/index.tsx
+++ tutorial-react-router-dom/src/components/About/index.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 
-export const Home = () => {
+export const About = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>About</h1>
     </div>
   );
 };
--- tutorial-react-router-dom/src/main.tsx
+++ tutorial-react-router-dom/src/main.tsx
@@ -4,8 +4,16 @@ import { Home } from "./components/Home";
 
 import "./styles/global.css";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { About } from "./components/About";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
